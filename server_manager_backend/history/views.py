0049db788ccb23757b31854214437920f16c981a
hasan foraty <hasanforaty19@gmail.com>
2024-06-08 09:42:48 +0330

    add support for query parameter in swagger

--- server_manager_backend/history/views.py
+++ server_manager_backend/history/views.py
@@ -1,9 +1,36 @@
-from rest_framework import mixins,viewsets
+from rest_framework import mixins, viewsets
 
 from history.models import ActionHistory
 from history.serializers import ActionHistorySerializer
 
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
 
+
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                name='server',
+                type=OpenApiTypes.STR,
+                description='id of The server ',
+                required=False,
+                location=OpenApiParameter.QUERY,
+            ),
+            OpenApiParameter(
+                name='action',
+                type=OpenApiTypes.STR,
+                description='id of the action in history',
+                required=False,
+                location=OpenApiParameter.QUERY,
+            ),
+        ]
+    )
+)
 class ActionHistoryViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
@@ -21,4 +48,3 @@ class ActionHistoryViewSet(
         if action is not None:
             queryset = queryset.filter(action_id=action)
         return queryset
-
